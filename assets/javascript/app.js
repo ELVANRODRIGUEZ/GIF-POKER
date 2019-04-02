b6fbbfbe518be4fe0e6b232a9541d2bae60cdaf2
ELVANRODRIGUEZ <elvanovich2000@yahoo.com.mx>
2019-04-02 09:45:49 -0600

    Almost done with the effects: 1) Add a subject 2) Press the coin to pay the fare 3) Hit the cards maze to show the cards.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,7 @@
 var gifPokerGame = {
+    gameOn: false,
+    cardsOnTable: false,
+    farePaid: false,
     inputSubject: "",
     selectedSubjectArr: [],
     selectedSubject: "",
@@ -36,12 +39,43 @@ var gifPokerGame = {
         }).then(function (resp) {
             console.log(resp);
             $(".gifImages").each(function (i) {
-                var randomResult = Math.floor(Math.random() * resp.data.length -1);
+                var randomResult = Math.floor(Math.random() * resp.data.length);
                 var url = resp.data[randomResult].images.original.url;
                 console.log(url);
                 $(this).attr("src", url);
             })
         })
+
+    },
+
+    displayHand: function () {
+        if (!this.cardsOnTable) {
+            $("#slot1").css("visibility", "visible");
+            $("#slot1").animate({
+                left: "+=50"
+            }, 100, function () {});
+            setTimeout(function () {
+                $("#slot2").css("visibility", "visible");
+                $("#slot2").animate({
+                    left: "+=200"
+                }, 200, function () {});
+            }, 100);
+            setTimeout(function () {
+                $("#slot3").css("visibility", "visible");
+                $("#slot3").animate({
+                    left: "+=350"
+                }, 300, function () {});
+            }, 300);
+            setTimeout(function () {
+                $("#slot4").css("visibility", "visible");
+                $("#slot4").animate({
+                    left: "+=500"
+                }, 400, function () {});
+            }, 600);
+
+            this.cardsOnTable = true;
+        }
+
     }
 }
 
@@ -49,12 +83,41 @@ $(document).on("click", ".cassinoToken", function () {
     gifPokerGame.selectedSubject = $(this).attr("subject");
     gifPokerGame.selectedSubject = gifPokerGame.selectedSubject.replace(/\s/g, "%20");
     gifPokerGame.retrieveSubject(gifPokerGame.selectedSubject);
+    gifPokerGame.farePaid = true;
+    $("#pokerMaze").css("filter", "drop-shadow(15px 15px 5px black)");
+    $("#pokerMaze").css("margin-right", "5px");
 
 })
 
-$(document).on("click", ".addSubject", function (event) {
+$(".addSubject").on("click", function (event) {
     event.preventDefault();
     gifPokerGame.inputSubject = $("#searcher").val().toUpperCase();
     gifPokerGame.addSubject(gifPokerGame.inputSubject);
 
+})
+
+$("#pokerMaze").on("click", function () {
+    if (gifPokerGame.farePaid) {
+        gifPokerGame.displayHand();
+        $(this).css("filter", "drop-shadow(5px 5px 5px black)");
+        $(this).css("margin-right", 0);
+        gifPokerGame.farePaid = false;
+    }
+})
+
+$(document).on("mouseover", ".newSubject", function () {
+    $(this).css("cursor", "not-allowed");
+    $(this).css("background-color", "red");
+
+})
+
+$(document).on("mouseout", ".newSubject", function () {
+    $(this).css("background-color", "rgb(0, 0, 0)");
+
+})
+
+$(document).on("click", ".newSubject", function () {
+    var subject = $(this).text();
+    $(this).remove();
+    $("img[subject*='" + subject + "']").remove();
 })
